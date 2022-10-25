ab985784c2f00e31ba35102388195872d35d1618
Tosyanchyk <ddd.tosha@gmail.com>
2022-10-25 23:52:14 +0300

    fetching data and some styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react' // Summary for component
+
 import Header from './components/Header/Header'
+import Converter from './components/Converter/Converter'
 
 import s from './App.module.sass'
 
 export default function App() {
-  return <>
+
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+
+  const currUrl = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
+
+  useEffect(() => {
+    fetch(currUrl)
+      .then(res => res.json())
+      .then(data => setCurrencyOptions(data))
+  }, [])
+
+  return <div className='container'>
     <Header />
-  </>
+    <div className={s.inner}>
+      <Converter
+        currencyOptions={currencyOptions}
+      />
+      <span>is</span>
+      <Converter
+        currencyOptions={currencyOptions}
+      />
+    </div>
+  </div >
 }
