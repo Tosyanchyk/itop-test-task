49535cdeaee3c11e0f49d52dbe236b5c53351c8f
Tosyanchyk <ddd.tosha@gmail.com>
2022-10-26 00:00:20 +0300

    updating component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'; 
+import s from './Header.module.sass'
 
 export default function Header(data) {
 
   const [exchange, setExchange] = useState(null)
 
+  const currUrl = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
+
   useEffect(() => {
-    fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+    fetch(currUrl)
       .then(res => res.json())
       .then(data => setExchange(data))
   }, [])
 
-  console.log(exchange)
 
-  return <>
 
-    {exchange &&
-      <ul>
-        {exchange.map((item, index) => {
-          return <li key={index}>
-            {item.ccy}
-          </li>
-        })}
-      </ul>
-    }
 
-    {/*
-      base_ccy: "UAH"
-      buy: "39.50000"
-      ccy: "USD"
-      sale: "39.90000"
-    */}
-  </>
+  return <header className={s.header}>
+
+    <div className={s.asd}>Exchange Rate</div>
+
+  </header>
 }
